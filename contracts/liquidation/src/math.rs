45630fa9836a8180556eb7a34cb3a245ba8a63ea
dmstjd0182 <dmstjd0182@snu.ac.kr>
2022-01-26 20:26:51 +0900

    added: new_exp(), zero(), Copy trait

--- contracts/liquidation/src/math.rs
+++ contracts/liquidation/src/math.rs
@@ -12,7 +12,7 @@ construct_uint! {
 
 const DECIMAL: u128 = 1_000_000_000_000;        //1e12
 
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Copy, Clone, Debug)]
 #[serde(crate = "near_sdk::serde")]
 pub struct D128 {
     pub num: U128,
@@ -38,6 +38,23 @@ impl D128 {
     /// get Decimal number 1.0
     pub fn one() -> Self {
         Self::default()
+    }
+
+    /// get Decimal number 0.0
+    pub fn zero() -> Self {
+        Self::new(0)
+    }
+
+    /// get decimal value
+    pub fn get_decimal() -> u32 {
+        Self::default().decimal
+    }
+
+    /// Returns num * (10**exp)
+    /// Ex) new_exp(1, -2) == D128::new(1e10 as u128) == (0.01)
+    pub fn new_exp(num: u128, exp: i32) -> Self {
+        assert!(exp >= -12);
+        Self::new(num * u128::pow(10, (Self::get_decimal() as i32 + exp) as u32))
     }
 }
 
@@ -93,6 +110,8 @@ impl Mul<D128> for D128 {
 
 impl Mul<u128> for D128 {
     type Output = u128;
+    /// NOTE: u128 value should be big integer or there may be round error.
+    /// Returns u128 value
     #[inline]
     fn mul(self, other: u128) -> u128 {
         (U256::from(other) * U256::from(self.num.0) 
