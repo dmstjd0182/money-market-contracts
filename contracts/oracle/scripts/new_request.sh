b1392bf5e0b79c06808c5cd3c36658e3b0722ca0
dmstjd0182 <dmstjd0182@snu.ac.kr>
2022-01-28 18:09:59 +0900

    tmp: update scripts

--- contracts/oracle/scripts/new_request.sh
+++ contracts/oracle/scripts/new_request.sh
@@ -16,7 +16,7 @@ while [ $# -gt 0 ]; do
   shift
 done
 
-JSON="{\"sources\": [],\"tags\":[\"sports\",\"nfl\"],\"description\":\"Which team won the NFL Super Bowl in 1996?\",\"outcomes\":[\"Cowboys\",\"Steelers\"],\"challenge_period\":\"120000000000\",\"settlement_time\":\"1\",\"data_type\":\"String\",\"creator\":\"$senderId\"}"
+JSON='{\"sources\": [ { \"end_point\": \"https://api.coingecko.com/api/v3/simple/price?ids=tether%2Cnear&vs_currencies=usd&include_last_updated_at=true\", \"source_path\":\"\"}], \"tags\":[\"pricing\",\"near\",\"tether\"],  \"challenge_period\":\"120000000000\", \"settlement_time\":\"1\", \"data_type\":\"String\", \"creator\":\"blockwave.testnet\"}'
 env NEAR_ENV=testnet near call $paymentToken ft_transfer_call "{\"amount\": \"1000000000000000000000000\", \"msg\": \"$JSON\", \"receiver_id\": \"$accountId\"}" --accountId $senderId --amount 0.000000000000000000000001 --gas=300000000000000
 
 # env NEAR_ENV=$network near call $paymentToken ft_transfer_call $ARGS --accountId $accountId --amount 0.000000000000000000000001 --gas=300000000000000
